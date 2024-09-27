2c07e2f873528e9127ace7bd7c73cb305176deab
ItsJuanda17 <1109115477@u.icesi.edu.co>
2024-09-26 23:47:10 -0500

    Fix: Se arregla error al desconectar un usuario de la sala

--- Base/ClientHandler.java
+++ Base/ClientHandler.java
@@ -60,7 +60,15 @@ public class ClientHandler implements Runnable {
         } catch (IOException e) {
             System.out.println(e.getMessage());
         } finally {
-            chatters.removeUser(username); // Eliminar el usuario al salir
+            try {
+                chatters.removeUser(username);
+                if (client != null) {
+                    client.close();
+                }
+               
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
         }
     }
 
